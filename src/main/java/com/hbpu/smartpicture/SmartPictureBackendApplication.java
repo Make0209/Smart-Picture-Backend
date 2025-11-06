2430155ff79c47de5f45d03a7ddcebcb38f33ee5
kefanM <1489512614@qq.com>
2025-11-06 17:31:02 +0800

    初始化框架

---
+++ src/main/java/com/hbpu/smartpicture/SmartPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hbpu.smartpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPictureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hbpu/smartpicture/SmartPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbpu.smartpicture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
