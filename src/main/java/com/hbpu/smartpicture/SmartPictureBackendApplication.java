f529b1ae0488e82048e02b5b7e6eda6dff3acce7
kefanM <1489512614@qq.com>
2025-11-07 17:08:15 +0800

    初始化框架

--- src/main/java/com/hbpu/smartpicture/SmartPictureBackendApplication.java
+++ src/main/java/com/hbpu/smartpicture/SmartPictureBackendApplication.java
@@ -2,8 +2,10 @@ package com.hbpu.smartpicture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class SmartPictureBackendApplication {
 
     public static void main(String[] args) {
