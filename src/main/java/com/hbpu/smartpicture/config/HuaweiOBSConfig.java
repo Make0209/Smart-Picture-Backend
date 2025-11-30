0d3106deb4494f8a10706ec9fcdd152a98483cb6
kefanM <1489512614@qq.com>
2025-11-30 18:23:02 +0800

    引入华为OBS对象存储

---
+++ src/main/java/com/hbpu/smartpicture/config/HuaweiOBSConfig.java
@@ -0,0 +1,28 @@
+package com.hbpu.smartpicture.config;
+
+import com.obs.services.ObsClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 华为对象存储配置类
+ */
+@Configuration
+@ConfigurationProperties(prefix = "huawei.obs")
+@Data
+public class HuaweiOBSConfig {
+    //访问地址
+    private String endPoint;
+    //访问key
+    private String ak;
+    //访问密钥
+    private String sk;
+
+    //当结束使用时自动关闭释放资源
+    @Bean(destroyMethod = "close")
+    public ObsClient obsClient() {
+        return new ObsClient(ak, sk, endPoint);
+    }
+}
